f66c8dac9bd56347a4442d121b32333d03b15b1f
n.zbiranik <n.zbiranik@cdek.ru>
2019-08-31 14:47:18 +0700

    add solution for 2-module 1-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -2,17 +2,15 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
-  #limit;
-  #size;
   constructor(options) {
     super(options);
-    this.#limit = options.limit ? options.limit : 0;
-    this.#size = 0;
+    this.limit = options.limit ? options.limit : 0;
+    this.size = 0;
   }
 
   _transform(chunk, encoding, callback) {
-    this.#size += chunk.length;
-    if (this.#size > this.#limit) {
+    this.size += chunk.length;
+    if (this.size > this.limit) {
       this.emit('error', new LimitExceededError());
     }
     this.push(chunk);
--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,30 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.objectMode = true;
   }
 
   _transform(chunk, encoding, callback) {
+    /* const newChunks = chunk.toString().split(os.EOL);
+    for (const newChunk of newChunks) {
+      this.push(newChunk);
+    }
+    callback(); */
+    let data = chunk.toString();
+    if (this._lastLineData) data = this._lastLineData + data;
+
+    const lines = data.split(os.EOL);
+    this._lastLineData = lines.splice(lines.length-1, 1)[0];
+
+    lines.forEach(this.push.bind(this));
+    callback();
   }
 
   _flush(callback) {
+    this.push(os.EOL);
+    if (this._lastLineData) this.push(this._lastLineData);
+    this._lastLineData = null;
+    callback();
   }
 }
 
