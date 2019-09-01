939ebf00e11c0513f3d89a70cf4659451995cfa4
n.zbiranik <n.zbiranik@cdek.ru>
2019-09-01 20:37:37 +0700

    add solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -7,9 +7,20 @@ class LineSplitStream extends stream.Transform {
   }
 
   _transform(chunk, encoding, callback) {
+    let data = chunk.toString();
+    if (this._lastLineData) data = this._lastLineData + data;
+
+    let lines = data.split(os.EOL);
+    this._lastLineData = lines.splice(lines.length-1, 1)[0];
+
+    lines.forEach(this.push.bind(this));
+    callback();
   }
 
   _flush(callback) {
+    if (this._lastLineData) this.push(this._lastLineData);
+    this._lastLineData = null;
+    callback();
   }
 }
 
