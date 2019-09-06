5c313d4db0666c14f6d8917ac83e1748e8e3ebb3
Збираник Наталия Валерьевна <n.zbiranik@cdek.ru>
2019-09-06 14:31:52 +0700

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,23 +1,47 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
-
   switch (req.method) {
     case 'DELETE':
-
+      const pathname = url.parse(req.url).pathname.slice(1);
+      if (pathname.indexOf('/') !== -1) {
+        res.statusCode = 400;
+        res.end('No sub folders in path');
+      }
+      let filepath = path.join(__dirname, 'files', pathname);
+      filepath = path.normalize(filepath);
+      fs.stat(filepath, function(er, stats) {
+        if (er || !stats.isFile()) {
+          res.statusCode = 404;
+          res.end('Not found');
+        } else {
+          deleteFile(filepath, res);
+        }
+      });
       break;
 
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      throw new Error();
   }
 });
 
+server.on('error', (req, res) => {
+  res.statusCode = 501;
+  res.end('Not implemented');
+});
+
+function deleteFile(filepath, res) {
+  fs.unlink(filepath, (err) => {
+    if (err) throw new Error();
+    res.statusCode = 200;
+    res.end('File deleted');
+  });
+}
+
 module.exports = server;
+
