2e0fb8cffd8ce38f8aadff7a021f44a7dfc6ce07
Збираник Наталия Валерьевна <n.zbiranik@cdek.ru>
2019-09-09 17:49:19 +0700

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,9 +1,11 @@
+const Product = require('../models/Product');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   ctx.body = {products: []};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {products: []};
+  ctx.body = {products: await Product.find()};
 };
 
 module.exports.productById = async function productById(ctx, next) {
