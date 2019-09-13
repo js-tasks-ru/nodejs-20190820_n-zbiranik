af637dd5a7016aca366fc51d6bbcd9f4f7be0834
Збираник Наталия Валерьевна <n.zbiranik@cdek.ru>
2019-09-13 16:10:10 +0700

    add solution for 7-module 1-task

--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -1,9 +1,21 @@
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../../models/User');
 
-module.exports = new LocalStrategy(
-    {session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+module.exports = new LocalStrategy({
+  session: false,
+  usernameField: 'email',
+  passwordField: 'password',
+},
+async (login, password, done) => {
+  const user = await User.findOne({email: login});
+  if (user === null) {
+    done(null, false, 'Нет такого пользователя');
+  } else {
+    const checkPassword = await user.checkPassword(password);
+    if (checkPassword === false) {
+      done(null, false, 'Невереный пароль');
+    } else {
+      done(null, user);
     }
-);
+  }
+});
