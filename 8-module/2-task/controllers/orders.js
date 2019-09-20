00f61ae6baa56d338d65775990aa8eae2e7af29a
Збираник Наталия Валерьевна <n.zbiranik@cdek.ru>
2019-09-20 10:30:33 +0700

    add solution for 8-module 2-task

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -2,7 +2,24 @@ const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const orderData = ctx.request.body;
+  orderData.user = ctx.user;
+
+  const order = new Order(orderData);
+
+  await order.save().then(() => {
+    sendMail({
+      template: 'order-confirmation',
+      locals: {id: order._id, product: order.product},
+      to: ctx.user.email,
+      subject: 'Новый заказ',
+    });
+    ctx.status = 200;
+    ctx.body = {order: order._id};
+  });
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  ctx.status = 200;
+  ctx.body = {orders: await Order.find({user: ctx.user})};
 };
